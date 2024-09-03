5871cb7101b692cc66a339f7b8359137dae79077
prihandiva <prihandiva1711@gmail.com>
2024-09-03 12:27:33 +0700

    first commit

---
+++ JS1/fulan.dart
@@ -0,0 +1,13 @@
+//FITRIA RAMADHANI PRIHANDIVA
+//JS1 - P.MOBILE
+void main() {
+  //
+  //for (int i = 0; i < 10; i++) {
+  //  print('hello ${i + 1}');
+  //}
+
+  //Soal 1
+  for (int i = 19; i > 9; i--) {
+    print('Nama saya adalah Fulan, sekarang saya berumur ${i - 1}');
+  }
+}
